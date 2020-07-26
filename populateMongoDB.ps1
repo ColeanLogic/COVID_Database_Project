f7652bf3223b6517a1c790fcc3d209ccbc590d7d
jackBonadies <jbonadies6@gmail.com>
2020-07-26 13:56:23 -0400

    json file to populate mongodb database. added script to load json files into mongodb

---
+++ populateMongoDB.ps1
@@ -0,0 +1,2 @@
+& "C:\Program Files\MongoDB\Server\4.2\bin\mongoimport.exe" --jsonArray --db covid_db --collection cases --file "cases.json"  
+& "C:\Program Files\MongoDB\Server\4.2\bin\mongoimport.exe" --jsonArray --db covid_db --collection county --file "county.json"  
