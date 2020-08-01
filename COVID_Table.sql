cd8413a5382cb370ea76e22382e60c25e02131da
Tom Birmingham <tombirmingham91@gmail.com>
2020-08-01 13:17:33 -0400

    Added AUTO_INCREMENT, and added formatting

--- COVID_Table.sql
+++ COVID_Table.sql
@@ -3,87 +3,87 @@ USE COVID_Database;
 
 CREATE TABLE login
 (
-    username VARCHAR(50)not null,
-    password VARCHAR(50) not null,
-    role VARCHAR(50) not null,
-    primary key(username)
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY(username)
 );
 
 CREATE TABLE county
 (
-    county_date VARCHAR (15) not null,
-    county_name VARCHAR(75) not null,
-    state_name VARCHAR(50) not null,
-    county_id NUMERIC(5,0) not null,
+    county_date VARCHAR (15) NOT NULL,
+    county_name VARCHAR(75) NOT NULL,
+    state_name VARCHAR(50) NOT NULL,
+    county_id NUMERIC(5,0) NOT NULL,
     cases NUMERIC(7,0),
     deaths NUMERIC(7,0),
-    primary key (county_id, county_date)
+    PRIMARY KEY (county_id, county_date)
 );
 
 CREATE TABLE patient
 (
-    patient_id NUMERIC(6,0) not null,
-    name VARCHAR(100) not null,
-    address_street VARCHAR(100) not null,
-    address_city VARCHAR(100) not null,
-    address_state VARCHAR(100) not null,
-    address_zip VARCHAR(100) not null,
-    phone VARCHAR(20) not null,
+    patient_id NUMERIC(6,0) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    address_street VARCHAR(100) NOT NULL,
+    address_city VARCHAR(100) NOT NULL,
+    address_state VARCHAR(100) NOT NULL,
+    address_zip VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
     admitted DATE,
     discharged DATE ,
-    county_id NUMERIC(5,0) not null,
+    county_id NUMERIC(5,0) NOT NULL,
     health_info VARCHAR(100),
-    age NUMERIC(3,0) not null,
+    age NUMERIC(3,0) NOT NULL,
     race VARCHAR(50),
-    gender VARCHAR(20) not null,
-    primary key(patient_id),
-    foreign key (county_id) references county(county_id)
+    gender VARCHAR(20) NOT NULL,
+    PRIMARY KEY(patient_id),
+    foreign key (county_id) REFERENCES county(county_id)
 );
 
 CREATE TABLE hospital
 (
-    hospital_id NUMERIC(6,0) not null,
-    name VARCHAR(75) not null,
-    county_id NUMERIC(5,0) not null,
-    primary key (hospital_id),
-    foreign key (county_id) references county(county_id)
+    hospital_id NUMERIC(6,0) NOT NULL AUTO_INCREMEMNT,
+    name VARCHAR(75) NOT NULL,
+    county_id NUMERIC(5,0) NOT NULL,
+    PRIMARY KEY (hospital_id),
+    foreign key (county_id) REFERENCES county(county_id)
 );
 
 CREATE TABLE case_no
 (
-    case_id NUMERIC(7,0) not null,
-    patient_id NUMERIC(6,0) not null,
-    county_id NUMERIC(5,0) not null,
-    hospital_id NUMERIC(6,0) not null,
+    case_id NUMERIC(7,0) NOT NULL AUTO_INCREMENT,
+    patient_id NUMERIC(6,0) NOT NULL,
+    county_id NUMERIC(5,0) NOT NULL,
+    hospital_id NUMERIC(6,0) NOT NULL,
     status VARCHAR(15),
     name VARCHAR(100),
-    primary key (case_id),
-    foreign key (county_id) references county(county_id),
-    foreign key (patient_id) references patient(patient_id),
-    foreign key (hospital_id) references hospital(hospital_id)
+    PRIMARY KEY (case_id),
+    foreign key (county_id) REFERENCES county(county_id),
+    foreign key (patient_id) REFERENCES patient(patient_id),
+    foreign key (hospital_id) REFERENCES hospital(hospital_id)
 );
 
-create view case_summary
-as
-    select status, COUNT(case_id)
-    from case_no
-    group by status;
+CREATE VIEW case_summary AS
+    SELECT status, COUNT(case_id)
+    FROM case_no
+    GROUP BY status;
 
-create view county_summary
-as
-    select county_name, state_name, SUM(cases), SUM(deaths)
-    from county
+CREATE VIEW county_summary AS
+    SELECT county_name, state_name, SUM(cases), SUM(deaths)
+    FROM county
     GROUP BY county_id;
 
-create view hospital_summary
-as
-    select name, status, COUNT(case_id)
-    from case_no inner join hospital on (case_no.hospital_id = hospital.hospital_id)
-    group by name, status;
+CREATE VIEW hospital_summary AS
+    SELECT name, status, COUNT(case_id)
+    FROM case_no inner join hospital on (case_no.hospital_id = hospital.hospital_id)
+    GROUP BY name, status;
 
-create view comprehensive_summary
-as
-    select name, status, county_name, state_name, cases, deaths
-    from case_no inner join hospital inner join county ON (case_no.hospital_id = hospital.hospital_id) 
+CREATE VIEW comprehensive_summary AS
+    SELECT name, status, county_name, state_name, cases, deaths
+    FROM case_no inner join hospital inner join county ON (case_no.hospital_id = hospital.hospital_id)
     WHERE case_no.county_id = county.county_id
     GROUP BY case_id;
+
+ALTER TABLE patient AUTO_INCREMENT=500775;
+ALTER TABLE hospital AUTO_INCREMENT=600030;
+ALTER TABLE case_no AUTO_INCREMENT=800302;
