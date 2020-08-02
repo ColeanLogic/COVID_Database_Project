15f3113f233b2278c581212d9105919dd1f7e761
David Boyer <bigcoffeemug@gmail.com>
2020-08-02 19:26:52 -0400

    stuff

--- flask_main/main.py
+++ flask_main/main.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import mysql.connector
 import pdb
 import os
-from forms import PatientFormCreate, HospitalFormCreate
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, RadioField
-from wtforms.validators import DataRequired
+import sys
+# from forms import PatientFormCreate
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField, TextAreaField, RadioField
+# from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.secret_key = b'helloworld'
 
-host = 'localhost'
+host = '127.0.0.1'
+# mongo_host = '127.0.0.1'
+# mongo_port = '20717'
 user = 'root'
 passwd = ''
 dbname= 'COVID_Database'
@@ -22,7 +25,6 @@ def connect_to_xampp(host,user,passwd,dbname):
     except mysql.connector.Error as E:
         print(E)
     return connection
-    
 def connect_to_db(con,dbname):
     csr = con.cursor()
     csr.execute("use " + dbname)
@@ -35,18 +37,17 @@ def create_tables():
     for res in csr.execute(sqlCmds,multi=True):
         pass
     con.commit()
-
 def load_tables():
     global con
     connect_to_db(con,dbname)
-    path_table = [('county_jul','county'),('hospital','hospital'),('login','login'),('patients_july','patient'),('case','case_no')]
+    path_table = [('county_jan_to_april','county'),('county_may','county'),('county_jun','county'),('county_jul','county'),('hospital','hospital'),('login','login'),('patients_april','patient'),('patients_may','patient'),('patients_june','patient'),('patients_july','patient'),('patients_march','patient'),('case','case_no')]
     for i in path_table:
         county_path = os.path.join(os.getcwd(), '..',i[0]+'.csv')
         county_path = county_path.replace(r"/mnt/c", r"C:")
         if(i[1] != 'patient'):
             load_data_query = "LOAD DATA INFILE '{0}' IGNORE INTO TABLE {1} FIELDS TERMINATED BY ',' IGNORE 1 LINES;".format(county_path,i[1])
         else:
-            load_data_query = "LOAD DATA INFILE '{0}' IGNORE INTO TABLE {1} FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES (patient_id, name, address,@dummy, @dummy, @dummy, phone, admitted, discharged, county_id, health_info, age,race,gender);".format(county_path,i[1])
+            load_data_query = "LOAD DATA INFILE '{0}' IGNORE INTO TABLE {1} FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES (patient_id, name, address_street, address_city, address_state, address_zip, phone, admitted, discharged, county_id, health_info, age,race,gender);".format(county_path,i[1])
         csr = con.cursor()
         csr.execute(load_data_query)
         con.commit()
@@ -63,12 +64,43 @@ def return_query(query):
     csr.execute(query)
     return csr.fetchall()
 
-# Connect to mysql database, create database if not created yet
+# this is the inster data query
+def insert_query(query):
+    global con
+    csr = con.cursor()
+    csr.execute(query)
+
+def connect_to_mongodb():
+    client = MongoClient('mongodb://127.0.0.1:27017');#MongoClient("mongodb://" + mongo_host+':'+mongo_port)
+    global mongo_con
+    if(mongo_con != None):
+        #already connected
+        return 
+    exists = False
+    for db in client.list_databases():
+        if db["name"] == "covid_db":
+          exists = True
+    if(exists==False):
+        raise Exception("You do not have a covid_db in your list of databases")
+    mongo_con = client.covid_db;
+    return
+
+# pdb.set_trace()
 con = connect_to_xampp(host,user,passwd,dbname)
+# mongo_con = None
 if not database_created():
     create_tables()
     load_tables()
 connect_to_db(con,dbname)
+# connect_to_mongodb()
+@app.route('/switch_db')
+def switch_db():
+    if "use_mongo" in session:
+        session.pop('use_mongo',None)
+    else:
+        session['use_mongo'] = True
+        connect_to_mongodb()
+    return redirect(url_for('home'));
 
 @app.route('/',methods=['GET', 'POST'])
 def home():
@@ -76,10 +108,41 @@ def home():
         session['usr'] = request.form['usr']
     return render_template('home.html')
     
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        username = request.form ['usr']
+        password = request.form ['pass']
+        qry = f'''SELECT * FROM login WHERE username="{username}";'''
+        res = return_query(qry)
+
+        print(res, file=sys.stderr)
+        session['usr'] = None
+        if len(res) == 0:
+            print("Incorrect username", file=sys.stderr)
+            return render_template('pass_fail.html')
+        elif res[0][1] != password: # This makes assumptions and is kinda yicky
+            print("Incorrect password", file=sys.stderr)
+            return render_template('usr_fail.html')
+        else:
+            session['usr'] = username
+        return redirect(url_for('home'))
     
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        session['usr'] = request.form['usr']
+        username = request.form ['usr']
+        password = request.form ['pass']
+        role = request.form ['role']
+        qry = f'''INSERT INTO login VALUES ("{username}","{password}","{role}");'''
+        res = insert_query(qry)
+        return redirect(url_for('home'))
+
+    return render_template('register.html')
+
 @app.route('/test')
 def test():
     strbuilder = ""
@@ -90,25 +153,36 @@ def logout():
     session.pop('usr',None)
     return redirect(url_for('home'))
 
-
 @app.route('/somewhere_else',methods=['POST'])
 def results_page():
     if request.method == 'POST':
         tblname = request.form['tablename']
         qry = "select * from " + tblname +";"
         res = return_query(qry)
-        return render_template('results.html',res=res,name=tblname)
-
-@app.route('/hooray', methods=['GET', 'POST'])
-def hooray():
-    return render_template('hooray.html')
-
-# Patient Routes
-@app.route('/patient_created/<new_patient_id>', methods=['GET', 'POST'])
-def patient_created(new_patient_id):
-    qry = f'''SELECT * FROM patient WHERE patient_id = "{new_patient_id}"'''
-    res = return_query(qry)
-    return render_template('patient_created.html', res=res)
+        return render_template('results.html',res=res,name=tblname);
+        
+@app.route('/chart',methods=['GET','POST'])
+def chart_page():
+    type1 = ""
+    if request.method == 'POST':
+        status = request.form['cases']
+        chart_data = {}
+        demographic = request.form['by']
+        statuses = mongo_con.cases.find({},{'status':1,'id':0}).distinct('status')
+        demographics = []
+        type1 = request.form['type']
+        if "age" in demographic:
+            demographics = [10,20,30,40,50,60,70]
+            for dem in demographics:
+                total = mongo_con.cases.find({"$and":[{'status':status},{'patient_info.' + demographic:dem}]}).count()
+                chart_data[dem] = total
+        else:
+            demographics = mongo_con.cases.find({},{'patient_info.'+demographic:1,'id':0}).distinct('patient_info.' + demographic)
+            for dem in demographics:
+                total = mongo_con.cases.find({"$and":[{'status':status},{'patient_info.' + demographic:dem}]}).count()
+                chart_data[dem] = total
+        return render_template('chart.html',dems=demographics,chart_data=chart_data,type1=type1)
+    return render_template('chart.html',chart_data=None,type1=type1)
 
 @app.route('/patient_create', methods=['GET', 'POST'])
 def patient_create():
@@ -134,26 +208,3 @@ def patient_create():
         csr.execute(qry)
         return redirect(f'/patient_created/{new_patient_id}.html')
     return render_template('patient_create.html', template_form=patient_form_create)
-
-
-#The following lines of code are detain the creation of a hospital
-@app.route('/hospital_created/<new_hospital_id>', methods=['GET', 'POST'])
-def hospital_created(new_hospital_id):
-    qry = f'''SELECT * FROM hospital WHERE hospital_id = "{new_hospital_id}"'''
-    res = return_query(qry)
-    return render_template('hospital_created.html', res=res)
-
-@app.route('/hospital_create', methods=['GET', 'POST'])
-def hospital_create():
-    global con
-    hospital_form_create = HospitalFormCreate()
-    if hospital_form_create.validate_on_submit():
-        new_hospital_id = hospital_form_create.hospital_id.data
-        new_name = hospital_form_create.name.data
-        new_county_id = hospital_form_create.county_id.data
-        qry = f'''INSERT INTO hospital (hospital_id, name, county_id) VALUES ({new_hospital_id},"{new_name}","{new_county_id}");'''
-        csr = con.cursor()
-        csr.execute(qry)
-        return redirect(f'/hospital_created/{new_hospital_id}.html')
-    return render_template('hospital_create.html', template_form=hospital_form_create)
-
